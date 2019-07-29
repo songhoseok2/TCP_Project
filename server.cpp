45e8f6147b903fbfa5b879ca15cee0414743a938
Ho Seok Song <hoseok@umich.edu>
2019-07-29 14:59:37 +0900

    Trimming

--- server.cpp
+++ server.cpp
@@ -1,61 +1,65 @@
 #include <iostream>
+#include <string>
 #include <WS2tcpip.h>
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
 
-int main()
+void exit_with_err_msg(const string msg)
 {
-	//Initialize Winsock
+	cout << msg << endl;
+	cout << "Cleaning WSA...";
+	WSACleanup();
+	cout << "Done. Exiting." << endl;
+	exit(1);
+}
+
+struct socket_info
+{
+	SOCKET sock;
+	sockaddr_in sock_addr;
+};
+
+void initialize_winsock()
+{
 	WSADATA wsData;
 	WORD ver = MAKEWORD(2, 2);
 
 	int ws_result = WSAStartup(ver, &wsData);
 	if (ws_result != 0)
 	{
-		cout << "Winsock initialization failed. Error " << ws_result << ". Exiting." << endl;
-		return 0;
+		exit_with_err_msg("Winsock initialization failed. Error " + to_string(ws_result) + ".");
 	}
+}
 
-	//Create a socket
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock == INVALID_SOCKET)
+socket_info create_listening_socket()
+{
+	socket_info listening_socket;
+
+	listening_socket.sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening_socket.sock == INVALID_SOCKET)
 	{
-		cout << "listen_socket creation failed. Error #" << WSAGetLastError() << ". Exiting." << endl;
-		return 0;
+		exit_with_err_msg("listen_socket creation failed. Error #" + to_string(WSAGetLastError()) + ".");
 	}
 
-	sockaddr_in serv_addr;
 	//setup the host_addr structure for use in bind call
+	listening_socket.sock_addr.sin_family = AF_INET;				//server byte order
+	listening_socket.sock_addr.sin_addr.S_un.S_addr = INADDR_ANY;	//automatically be filled with current host's IP address
+	listening_socket.sock_addr.sin_port = htons(54000);				//convert short integer value for port must be converted into network byte order
+	
+	return listening_socket;
+}
 
-	serv_addr.sin_family = AF_INET;					//server byte order
-	serv_addr.sin_addr.S_un.S_addr = INADDR_ANY;	//automatically be filled with current host's IP address
-	serv_addr.sin_port = htons(54000);				//convert short integer value for port must be converted into network byte order
-
-
-	//Bind ip address and port to a socket
-	if (bind(listen_sock, (sockaddr*)& serv_addr, sizeof(serv_addr)) < 0)
-	{
-		cout << "Binding failed. Exiting." << endl;
-		return 0;
-	}
-
-	//listen() tells the socket to listen to the incoming connections.
-	//The listen() function places all incoming connection into a backlog queue
-	//until accept() call accepts the connection.
-	//Setting the maximum size for the backlog queue to a defined number.
-	listen(listen_sock, SOMAXCONN);
-
-
+socket_info connect_to_a_client(socket_info listening_socket)
+{
 	//Wait for connection
-	sockaddr_in client_addr;
-	int client_size = sizeof(client_addr);
-	SOCKET client_sock = accept(listen_sock, (sockaddr*)& client_addr, &client_size);
+	socket_info client_socket;
+	int client_size = sizeof(client_socket.sock_addr);
+	client_socket.sock = accept(listening_socket.sock, (sockaddr*)& client_socket.sock_addr, &client_size);
 
-	if (client_sock == INVALID_SOCKET)
+	if (client_socket.sock == INVALID_SOCKET)
 	{
-		cout << "client_socket creation failed. Exiting" << endl;
-		return 0;
+		exit_with_err_msg("client_socket creation failed.");
 	}
 
 	char host[NI_MAXHOST];			//Client's remote name
@@ -66,27 +70,46 @@ int main()
 
 
 	cout << host << "connected on port ";
-	if (getnameinfo((sockaddr*)& client_sock, client_size, host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	if (getnameinfo((sockaddr*)& client_socket.sock, client_size, host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
 	{
-		cout <<  service << endl;
+		cout << service << endl;
 		cout << "getnameinfo successful." << endl;
 	}
 	else
 	{
-		inet_ntop(AF_INET, &client_addr.sin_addr, host, NI_MAXHOST);
-		cout << ntohs(client_addr.sin_port) << endl;
+		inet_ntop(AF_INET, &client_socket.sock_addr.sin_addr, host, NI_MAXHOST);
+		cout << ntohs(client_socket.sock_addr.sin_port) << endl;
 		cout << "getnameinfo failed." << endl;
 	}
 
-	//Close listening socket
-	closesocket(listen_sock);
+	return client_socket;
+}
+
+int main()
+{
+	initialize_winsock();
+	socket_info listening_socket = create_listening_socket();
+
+	//Bind ip address and port to a socket
+	if (bind(listening_socket.sock, (sockaddr*)& listening_socket.sock_addr, sizeof(listening_socket.sock_addr)) < 0)
+	{
+		exit_with_err_msg("Binding listening socket failed.");
+	}
+
+	//listen() tells the socket to listen to the incoming connections.
+	//The listen() function places all incoming connection into a backlog queue
+	//until accept() call accepts the connection.
+	//Setting the maximum size for the backlog queue to a defined number.
+	listen(listening_socket.sock, SOMAXCONN);
+
+	socket_info client_socket = connect_to_a_client(listening_socket);
 
 	//While loop to accept and echo message back to client
 	char buff[4096];
 	for(ZeroMemory(buff, 4096); true; ZeroMemory(buff, 4096))
 	{
 		//wait for client to send data
-		int bytes_received = recv(client_sock, buff, 4096, 0);
+		int bytes_received = recv(client_socket.sock, buff, 4096, 0);
 		if (bytes_received == SOCKET_ERROR)
 		{
 			cout << "ERROR in recv(). Exiting." << endl;
@@ -101,7 +124,7 @@ int main()
 		cout << "Received: " << buff << endl;
 
 		//echo message back to client
-		int bytes_sent = send(client_sock, buff, bytes_received + 1, 0); //+1 cuz of the terminating \0
+		int bytes_sent = send(client_socket.sock, buff, bytes_received + 1, 0); //+1 cuz of the terminating \0
 		if (bytes_sent == SOCKET_ERROR)
 		{
 			cout << "ERROR in send(). Exiting." << endl;
@@ -114,10 +137,7 @@ int main()
 		}
 	}
 
-	//Close the socket
-	closesocket(client_sock);
-
-	//Cleanup Winsock
+	closesocket(client_socket.sock);
 	WSACleanup();
 
 	return 0;
